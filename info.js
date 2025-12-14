ba21d30b2343acc168f8b1610f950c1452511208
RajabDev <rajabdev@RajabDevs-MacBook-Air.local>
2025-12-15 00:31:49 +0300

    first release v1.0

--- info.js
+++
@@ -1,4 +0,0 @@
-const ua = navigator.userAgent;
-document.getElementById("platform").textContent = "PlayStation 4";
-const fw = ua.match(/PlayStation 4\/([0-9.]+)/);
-document.getElementById("fw").textContent = fw ? fw[1] : "Unknown";
